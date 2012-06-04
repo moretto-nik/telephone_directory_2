3f9285dd96ce239b477c9c183b0211ffa0ff91cf
Pietro Di Bello <orione-team+dip@xpeppers.com>
2012-06-04 17:50:42 +0200

    second commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,15 @@ class UsersController < ApplicationController
     @users=User.search(params[:input_search])
   end
   
+  # GET /users/new
+  # GET /users/new.json
+  def new
+    @user = User.new
+  end
   
+  # POST /users
+  # POST /users.json
+  def create
+    @user = User.new(params[:user])
+  end
 end
